e49de12c8152e8042f82d5133715cfe1aeea576b
Jennifer Killian <ms.jenniferkillian@gmail.com>
2021-03-30 12:43:27 -0400

    Delete nutriboom directory

--- nutriboom/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-    return (
-        <footer>
-            <p id="fine-print">Copyright 2021, All rights reserved</p>
-        </footer>
-    )
-}
